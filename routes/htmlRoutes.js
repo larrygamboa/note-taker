cddd76cbbb1e11909d0ad601506ebd872258f538
Larry Gamboa <techmedia.lg@gmail.com>
2020-12-19 21:17:14 -0800

    fixing htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const path = require("path");
 
+// ============================================================
+// Routes user to notes.html page
 router.get("/notes", (req,res) => {
     res.sendFile(path.join(__dirname,"../public/notes.html"));
 });
