b035a4a058a1021150093674c375341f38c9eb03
Larry Gamboa <techmedia.lg@gmail.com>
2020-12-19 20:55:02 -0800

    fix syntax on server.js

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-//get necessary dependencies
+//Necessary dependencies
 const express = require("express");
 const path = require("path");
 
-//define PORT
+//Define PORT
 const PORT = process.env.PORT || 3000;
 
-//create instance of express server - object
+//Create instance of express server - object
 const server = express();
 
-//body parser
+//Body parser
 server.use(express.json());
 server.use(express.urlencoded({extended: true}));
 
-//server need to share the public folder to the client
+//Server need to share the public folder to the client
 server.use(express.static(path.join(__dirname,"/public")));
 
-//attch routes
+//Attach routes
 server.use(require("./routes/htmlRoutes.js"));
 server.use(require("./routes/apiRoutes.js"));
 
-//start server
+//Start server
 server.listen(PORT, () => {
-    console.log(`Listenidng to PORT: ${PORT}`);
+    console.log(`Listening to PORT: ${PORT}`);
 });
